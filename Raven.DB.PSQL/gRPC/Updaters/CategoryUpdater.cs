fbf4b86a4a82ae1bce8bc5a962cf027cb72bf02c
Артур Силкин <mrwhite19841@gmail.com>
2025-02-24 00:36:32 +0600

    Закончил с CategoryService

---
+++ Raven.DB.PSQL/gRPC/Updaters/CategoryUpdater.cs
@@ -0,0 +1,31 @@
+﻿using Raven.DB.PSQL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.DB.PSQL.gRPC.Updaters
+{
+    public class CategoryUpdater
+    {
+        public static async Task<(string, Categories)> UpdateCategory(Categories category)
+        {
+            (string, Categories) response = new();
+            try
+            {
+                using (var db = new AppDbContext())
+                {
+                    response.Item2 = db.Categories.Update(category).Entity;
+                    await db.SaveChangesAsync();
+                    response.Item1 = $"OK";
+                }
+                return response;
+            }
+            catch (Exception ex)
+            {
+                return (ex.Message, null);
+            }
+        }
+    }
+}
--- Raven/Services/CategoryService.cs
+++ Raven/Services/CategoryService.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity.Data;
 using Raven.DB.PSQL.Entity;
 using Raven.DB.PSQL.gRPC.Exporters;
 using Raven.DB.PSQL.gRPC.Importers;
+using Raven.DB.PSQL.gRPC.Updaters;
 
 namespace Raven.Services
 {
@@ -144,6 +145,101 @@ namespace Raven.Services
                         
             return Task.FromResult(response);
         }
-        //ToDo добавить остальные методы
+
+        public override Task<UpdateCategoryResponse> UpdateCategory(UpdateCategoryRequest request, ServerCallContext context)
+        {
+            UpdateCategoryResponse response = new UpdateCategoryResponse();
+            if(request.Id == 0)
+            {
+                response.CategoryMessage = null;
+                response.Code = 500;
+                response.Message = "Id было 0";
+                return Task.FromResult(response);
+            }
+            if (request.Image.Length != 0)
+            {
+                var minioResponse = Importer.AddNewCategoryImage(request.Image.ToByteArray());
+                if (minioResponse.IsCanceled)
+                {
+                    response.CategoryMessage = null;
+                    response.Code = 500;
+                    response.Message = minioResponse.Result.Item1;
+                }
+                else if (minioResponse.Result.Item2.Equals(null))
+                {
+                    response.CategoryMessage = null;
+                    response.Code = 500;
+                    response.Message = minioResponse.Result.Item1;
+                }
+                else
+                {
+                    var dbResponse = CategoryUpdater.UpdateCategory(new Categories()
+                    {
+                        Id = (int)request.Id,
+                        Title = request.Title,
+                        ImageFile = minioResponse.Result.Item2 ?? Guid.Empty
+                    });
+                    if (dbResponse.IsCanceled)
+                    {
+                        response.CategoryMessage = null;
+                        response.Code = 500;
+                        response.Message = dbResponse.Result.Item1;
+                    }
+                    else if (dbResponse.Result.Item1 != "OK")
+                    {
+                        response.CategoryMessage = null;
+                        response.Code = 500;
+                        response.Message = dbResponse.Result.Item1;
+                    }
+                    else
+                    {
+                        response.CategoryMessage = new CategoryMessage()
+                        {
+                            Id = (uint)dbResponse.Result.Item2.Id,
+                            Title = dbResponse.Result.Item2.Title,
+                            ImageFile = dbResponse.Result.Item2.ImageFile.ToString(),
+                            PostCount = (uint)dbResponse.Result.Item2.PostCount
+                        };
+                        response.Code = 200;
+                        response.Message += dbResponse.Result.Item1;
+                    }
+                }
+            }
+            else
+            {
+                var dbResponse = CategoryUpdater.UpdateCategory(new Categories() 
+                { 
+                    Id = (int)request.Id,
+                    Title = request.Title 
+                });
+                if (dbResponse.IsCanceled)
+                {
+                    response.CategoryMessage = null;
+                    response.Code = 500;
+                    response.Message = dbResponse.Result.Item1;
+                }
+                else if (dbResponse.Result.Item2 == null)
+                {
+                    response.CategoryMessage = null;
+                    response.Code = 500;
+                    response.Message = dbResponse.Result.Item1;
+                }
+                else
+                {
+                    response.CategoryMessage = new CategoryMessage()
+                    {
+                        Id = (uint)dbResponse.Result.Item2.Id,
+                        Title = dbResponse.Result.Item2.Title,
+                        ImageFile = dbResponse.Result.Item2.ImageFile.ToString(),
+                        PostCount = (uint)dbResponse.Result.Item2.PostCount
+                    };
+                    response.Code = 200;
+                    response.Message += dbResponse.Result.Item1;
+                }
+            }
+
+            return Task.FromResult(response);
+        }
+
     }
 }
