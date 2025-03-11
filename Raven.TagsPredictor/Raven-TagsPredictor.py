562d1ede7d4209792c0dc1ef8c2af429ff486df4
Артур Силкин <mrwhite19841@gmail.com>
2025-03-11 16:02:43 +0600

    Добавил контейнер python, который будет использоваться для подбора тегов

--- Raven.TagsPredictor/Raven-TagsPredictor.py
+++ Raven.TagsPredictor/Raven-TagsPredictor.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from pyexpat.errors import messages
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     print("API is work", flush=True)
+    return {"Response"}
